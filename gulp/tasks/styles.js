31deda45cb4dc9ab928dfaf2fc6977b536ac3ccd
YuriFA <yurik1452@mail.ru>
2017-08-09 18:55:18 +0300

    initial

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,17 @@
+import gulp from 'gulp';
+import cleanCSS from 'gulp-clean-css';
+import less from 'gulp-less';
+import prefix from 'gulp-autoprefixer';
+import rename from 'gulp-rename';
+import paths from '../paths';
+
+gulp.task('styles', () => {
+  gulp.src(`${paths.src.styles}/**/*.less`)
+    .pipe(less())
+    .pipe(prefix({
+      browsers: ['last 15 versions'],
+    }))
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(paths.dist.styles));
+});
