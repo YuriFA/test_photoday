50e9da4f641c2a1072bc6e7c00ad1f7975f7673d
YuriFA <yurik1452@mail.ru>
2017-08-10 13:26:58 +0300

    page wrapper changes

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,6 +1,7 @@
 import gulp from 'gulp';
 import cleanCSS from 'gulp-clean-css';
 import less from 'gulp-less';
+import concat from 'gulp-concat';
 import prefix from 'gulp-autoprefixer';
 import rename from 'gulp-rename';
 import paths from '../paths';
@@ -8,6 +9,7 @@ import paths from '../paths';
 gulp.task('styles', () => {
   gulp.src(`${paths.src.styles}/**/*.less`)
     .pipe(less())
+    .pipe(concat('main.css'))
     .pipe(prefix({
       browsers: ['last 15 versions'],
     }))
